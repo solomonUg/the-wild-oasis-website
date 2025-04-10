9b29ca374e51a041c3284aea14d593d17485739e
SolomonUg <solomonchris700@gmail.com>
2025-04-10 22:50:36 +0100

    modified caching in cabins page and about page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,14 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
-import Spinner from "../_components/Spinner";
 import SpinnerMini from "../_components/SpinnerMini";
+
+export const revalidate = 3600;
+// export const revalidate = 0;
+
+export const metadata = {
+  title: "Cabins"
+}
+
 
 export default function Page() {
 
@@ -15,7 +22,7 @@ export default function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
